377011b05dd364e9ba3b40690a1cac8d1ab2cb02
Habert <habertotieno4@gmail.com>
2025-03-23 22:26:55 +0300

    added flatacuties

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+fetch('http://localhost:3000/characters' )
+.then((res)=>(res.json()
+))
+.then(data=>(displayCharacters(data))
+
+
+)
+function displayCharacters(characters) {
+    let characterBar= document.querySelector("#character-bar")
+    characterBar.addEventListener ('mouseover',displayImages)
+
+    characters.forEach(character => {
+         let name =`<span>
+     ${character.name}
+     </span>
+    `
+    characterBar.innerHTML+= name
+})}
+function displayImages( characters) { let detailInfo=document.querySelector("#detail-info")
+
+
+characters.forEach((character)=>{
+    let image = `<img src="${character.image}"alt="${character.name}">`
+    detailInfo.innerHTML+=image
+})
+
+}
+
+
+
+
+
+fetch('http://localhost:3000/characters/1')
+.then((res)=>(res.json()))
+.then((data)=>{displayImages(data)})
+
+
