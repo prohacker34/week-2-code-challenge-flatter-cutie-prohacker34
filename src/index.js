3f521f43315bd802c4739326e95ac37082bd78f9
Habert <habertotieno4@gmail.com>
2025-03-25 10:43:36 +0300

    added submit

--- src/index.js
+++ src/index.js
@@ -7,22 +7,32 @@ fetch( baseUrl)
 .then((characters)=>{
     let characterBar=document.getElementById("character-bar")
     let img=document.getElementById("image")
+let newSpan=document.getElementById("vote-count")
 
 
     characters.forEach(character => {
     let span=document.createElement('span')
     characterBar.appendChild(span)
      span.textContent= character.name
-
+      newSpan.textContent=character.votes
      span.addEventListener('click',()=>(img.src=character.image
-
-
 
      ))
 
     });
 })
-let form=getElementById('')
+let form=document.getElementById("votes-form")
+form.addEventListener('submit',event=>{
+    event.preventDefault()
+    let votesInput=document.getElementById("votes")
+    let voteCount=document.getElementById("vote-count")
+    let currentVotes= parseInt(voteCount.textContent )
+    let newVotes=currentVotes+parseInt(votesInput.value )
+    voteCount.textContent=newVotes
+    form.reset()
+
+})
+
 
 
 
